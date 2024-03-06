15b29e1e2839cb3ff069a256703b6f2e5f30bfbd
gidimutai <giddymutai@gmail.com>
2024-03-06 21:52:16 +0300

    Add files via upload

---
+++ modern-javascript/01-variable-data-types/01-console/sript.js
@@ -0,0 +1,25 @@
+console.log(100);
+
+console.log('hello world')
+
+console.log(20, 'Hello', true)
+
+const x = 100;
+
+console.log(x)
+
+console.error('error')
+
+console.warn('warning');
+
+console.table({name:'Benji', email:'benji@gmail.com'})
+
+console.group('first'); 
+console.log('hi');
+console.log(x)
+console.error('error')
+console.warn('warning')
+console.groupEnd();
+
+const styles = 'padding: 10px; color: green; background-color: black ';
+console.log('%cHello world', styles);
