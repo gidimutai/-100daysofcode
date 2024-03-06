6097f98415eb0a56b598d530d51f2f41ea19e338
gidimutai <giddymutai@gmail.com>
2024-03-06 21:39:41 +0300

    Add files via upload

---
+++ modern-javascript/01-variable-data-types/04-data_types/script.js
@@ -0,0 +1,16 @@
+//string
+const firstName = 'Aisha'
+
+//number
+const age = 30 //number
+const temp = 98.9 //number
+
+//boolean
+const hasKids = true
+
+//Null
+const aptNumber = null;
+
+const output = aptNumber;
+
+console.log(output, typeof output);
